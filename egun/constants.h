7483fe880b1c52de180fbee929f02081311f2231
Martino Ferrari <martinogiordano.ferrari@iter.org>
2023-10-31 12:12:58 +0100

    added all bodies and their constants

--- egun/constants.h
+++ egun/constants.h
@@ -4,26 +4,41 @@
 
 namespace Density {
 const double Tungsten = 1.93e4;
+const double ImprTungsten = 0.8 * Tungsten;
 const double Alumina = 1.93e4;
+const double Molybdenum = 1.022e4;
 }; // namespace Density
 
 namespace SpecificHeat {
 const double Alumina = 134.4;
 const double Tungsten = 134.4;
+const double ImprTungsten = 142;
+const double Molybdenum = 251;
 }; // namespace SpecificHeat
 
 namespace Conductivity {
 const double Alumina = 170;
 const double Tungsten = 170;
+const double ImprTungsten = 170;
+const double Molybdenum = 138;
+const double Oil = 100;
 }; // namespace Conductivity
 
 namespace Emissivity {
 const double Tungsten = 0.5;
+const double ImprTungsten = 0.6;
 const double Alumina = 0.5;
+const double Molybdenum = 0.4;
+const double Oil = 1;
 }; // namespace Emissivity
 
 namespace def {
 const unsigned int n_filamets = 8;
+const unsigned int n_lower_shields = 4;
+const unsigned int n_upper_shields = 3;
+const unsigned int n_em_segments = 3;
+const unsigned int n_inner_bodies = 10;
+const unsigned int n_outer_bodies = 11;
 
 const double fil_masses[2] = {
     Density::Tungsten * 1.03e-7,
@@ -41,5 +56,30 @@ const double alumina_fialemnt[2] = {
     radiationResistance(Emissivity::Tungsten, 644.40e-6, Emissivity::Alumina,
                         1027.9e-6),
 };
+
+const double ls_masses[n_lower_shields] = {
+    Density::Molybdenum * 568.1971e-9,
+    Density::Molybdenum * 568.1971e-9,
+    Density::Molybdenum * 568.1971e-9,
+    Density::Molybdenum * 568.1971e-9,
+};
+
+const double us_masses[n_upper_shields] = {
+    Density::Molybdenum * 3089e-9,
+    Density::Molybdenum * 3615.5426e-9,
+    Density::Molybdenum * 4816.7602e-9,
+};
+
+const double ns_mass = Density::Molybdenum * 76004.12e-9;
+const double pr_mass = Density::Molybdenum * 73254.25e-9;
+
+const double em_masses[n_em_segments] = {
+    Density::ImprTungsten * 1,
+    Density::ImprTungsten * 1,
+    Density::ImprTungsten * 1,
+};
+
+const double oil_nosecone = radiationResistance(Emissivity::Molybdenum, 3.8232e-2, Emissivity::Oil, 1);
+const double oil_prolongator = radiationResistance(Emissivity::Molybdenum, 4.18842e-2, Emissivity::Oil, 1); 
 }; // namespace def
 #endif
--- egun/main.cpp
+++ egun/main.cpp
@@ -5,11 +5,32 @@
 
 using namespace lptm;
 
+exchange_t thermoionic_cooling(body_t &emitter, f64 &Ik) {
+  return [&emitter, &Ik](system_t &sys) -> void {
+    // formula to compute dE from Ik and emitter temeperature
+  };
+}
+exchange_t filament_heating(body_t &filament, f64 &Pf) {
+  return
+      [&filament, &Pf](system_t &sys) -> void { sys.heats[filament.id] += Pf; };
+}
+
 int main() {
+  const f64 dt = 0.001;      // s
+  const f64 sim_time = 10.0; // s
+
   system_t system;
 
   body_t filaments[def::n_filamets];
   body_t aluminas[def::n_filamets];
+  body_t lower_shields[def::n_lower_shields];
+  body_t upper_shields[def::n_upper_shields];
+  body_t oil, nosecone, prolongator;
+
+  std::vector<body_t> inner_cavity;
+  std::vector<body_t> outer_cavity;
+
+  // initialisation filaments and aluminas bodies and exchanges
   for (u32 i = 0; i < def::n_filamets; i++) {
     filaments[i] = body(def::fil_masses[i % 2], SpecificHeat::Tungsten,
                         Conductivity::Tungsten, 2000);
@@ -19,23 +40,72 @@ int main() {
     add_body(system, aluminas[i]);
     add_exchange(system, radiation(filaments[i], aluminas[i], 1.0,
                                    def::alumina_fialemnt[i % 2]));
+    inner_cavity.push_back(aluminas[i]);
   }
 
-  f64 dt = 0.001;            // s
-  const f64 sim_time = 10.0; // s
-  f64 time = 0;              // s
+  // initialisation lower shields bodies
+  for (u32 i = 0; i < def::n_lower_shields; i++) {
+    lower_shields[i] = body(def::ls_masses[i], SpecificHeat::Molybdenum,
+                            Conductivity::Molybdenum, 1000);
+    add_body(system, lower_shields[i]);
+    outer_cavity.push_back(lower_shields[i]);
+  }
+  
+  // initialisation nosecone and prolongator
+  nosecone = body(def::ns_mass, SpecificHeat::Molybdenum,
+                  Conductivity::Molybdenum, 1000);
+  prolongator = body(def::pr_mass, SpecificHeat::Molybdenum,
+                     Conductivity::Molybdenum, 1000);
+  add_body(system, nosecone);
+  add_body(system, prolongator);
+  outer_cavity.push_back(nosecone);
+  outer_cavity.push_back(prolongator);
+  
+  // initialisation upper shields bodies
+  for (u32 i = 0; i < def::n_upper_shields; i++) {
+    upper_shields[i] = body(def::us_masses[i], SpecificHeat::Molybdenum,
+                            Conductivity::Molybdenum, 1000);
+    add_body(system, upper_shields[i]);
+    outer_cavity.push_back(upper_shields[i]);
+  }
+
+  // initialise oil
+  oil = constant_temperature_body(900, Conductivity::Oil);
+  add_body(system, oil);
+
+  // add oil - nosecone / prolongator exchanges
+  add_exchange(system, radiation(nosecone, oil, 1, def::oil_nosecone));
+  add_exchange(system, radiation(prolongator, oil, 1, def::oil_prolongator));
+
+  // initialisation emitter bodies
+  body_t emitter[def::n_em_segments];
+  for (u32 i = 0; i < def::n_em_segments; i++) {
+    emitter[i] = body(def::em_masses[i], SpecificHeat::ImprTungsten,
+                      Conductivity::ImprTungsten, 1200);
+    add_body(system, emitter[i]);
+  }
+  outer_cavity.push_back(emitter[0]);
+  outer_cavity.push_back(emitter[0]);
+
+  /*******************/
+  /*   SYSTEM INFO   */
+  /*******************/
+  std::cout << "N Bodies: " << system.bodies.size() << "\n";
+  std::cout << "N Exchanges: " << system.exchanges.size() << "\n";
+  std::cout << "Outer cavity N Bodies: "<< outer_cavity.size() << "\n";
+  std::cout << "Inner cavity N Bodies: "<< inner_cavity.size() << "\n";
+  std::cout << "Static dt: " << dt << "s\n";
+  std::cout << "Simulation length: " << sim_time << "s\n";
+
+  /*******************/
+  /* SIMULATE SYSTEM */
+  /*******************/
+
+  f64 time = 0; // s
   u32 iteration = 0;
   while (time < sim_time) {
     evaluate(system, dt);
     time += dt;
   }
-  for (u32 i = 0; i < def::n_filamets; i++) {
-    std::cout << system.temperatures[filaments[i].id] << ", ";
-  }
-  std::cout << std::endl;
-  for (u32 i = 0; i < def::n_filamets; i++) {
-    std::cout << system.temperatures[aluminas[i].id] << ", ";
-  }
-  std::cout << std::endl;
   return 0;
 }
