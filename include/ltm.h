cb7c57d52ea0b71b3409a63b3c40be648bba3781
Martino Ferrari <martinogiordano.ferrari@iter.org>
2023-11-09 08:29:15 +0100

    fix: better api

--- include/ltm.h
+++ include/ltm.h
@@ -12,37 +12,37 @@ typedef int64_t i64;
 typedef double f64;
 typedef float f32;
 
-typedef struct {
+typedef struct ltm_body_t {
   char label[256];
   void *self;
-  void (*update)(void *, f64);
-  void (*add_heat)(void *, f64);
-  f64 (*temperature)(void *);
-  f64 (*heat)(void *);
-  void (*destroy)(void *);
+  void (*update)(struct ltm_body_t *, f64);
+  void (*add_heat)(struct ltm_body_t *, f64);
+  f64 (*temperature)(struct ltm_body_t *);
+  f64 (*heat)(struct ltm_body_t *);
+  void (*destroy)(struct ltm_body_t *);
 } ltm_body_t;
 
 ltm_body_t *ltm_body(const char *label, void *payload,
-                     void (*update)(void *, f64), void (*add_heat)(void *, f64),
-                     f64 (*temperature)(void *), f64 (*heat)(void *),
-                     void (*destroy)(void *));
+                     void (*update)(struct ltm_body_t *, f64),
+                     void (*add_heat)(struct ltm_body_t *, f64),
+                     f64 (*temperature)(struct ltm_body_t *),
+                     f64 (*heat)(struct ltm_body_t *),
+                     void (*destroy)(struct ltm_body_t *));
 
 ltm_body_t *ltm_dyn_body(const char *label, f64 T0, f64 mass, f64 spec_heat);
 
 ltm_body_t *ltm_const_body(const char *label, f64 T0);
 
-typedef struct {
+typedef struct ltm_exchange_t {
   void *self;
-  void (*eval)(void *);
-  void (*destroy)(void *);
+  void (*eval)(struct ltm_exchange_t *);
+  void (*destroy)(struct ltm_exchange_t *);
 } ltm_exchange_t;
 
 ltm_exchange_t *ltm_conduction(ltm_body_t *a, ltm_body_t *b,
                                f64 thermal_resistance);
 ltm_exchange_t *ltm_radiation(ltm_body_t *a, ltm_body_t *b,
                               f64 thermal_resistance);
-
-
 
 typedef struct {
   char label[256];
@@ -59,7 +59,7 @@ void ltm_sys_add_exchange(ltm_system_t *sys, ltm_exchange_t *exch);
 void ltm_sys_evaluate(ltm_system_t *sys, f64 dt);
 void ltm_sys_print(ltm_system_t *sys);
 void ltm_sys_csv_header(ltm_system_t *sys, FILE *fptr);
-void ltm_sys_to_csv(ltm_system_t *sys, FILE *fptr) ;
+void ltm_sys_to_csv(ltm_system_t *sys, FILE *fptr);
 FILE *ltm_sys_bin_file(ltm_system_t *sys, const char *fname);
 void ltm_sys_to_bin(ltm_system_t *sys, FILE *fptr);
 #endif
